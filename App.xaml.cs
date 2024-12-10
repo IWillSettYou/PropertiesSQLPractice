fda549958259bd8667287bec45d2900540da005c
iwillsettyou <bmate20050911@gmail.com>
2024-12-10 08:10:10 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PropertiesSQLPractice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
